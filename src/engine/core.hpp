cb35894fa9cce077fe40198b4c22796d3f508a3e
OdnetninI <odnetnininds@gmail.com>
2017-03-19 20:33:11 +0100

    Core Files

---
+++ src/engine/core.hpp
@@ -0,0 +1,11 @@
+#ifndef __20170319_CORE_HPP__
+#define __20170319_CORE_HPP__
+
+#include "engine/display/display.hpp"
+#include "engine/audio/audio.hpp"
+#include "engine/states/states.hpp"
+#include "engine/resources/resources.hpp"
+#include "engine/util/util.hpp"
+#include "engine/script/script.hpp"
+
+#endif /* __20170319_CORE_HPP__ */
